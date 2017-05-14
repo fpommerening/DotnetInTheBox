dc3931ba668575404d667100f8f18588b8a50700
Frank Pommerening <frank@pommerening-online.de>
2017-05-14 15:58:49 +0200

    Add debug code

---
+++ Samples/Webhook/src/Service/Bootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Nancy;
+using Nancy.Configuration;
+
+namespace FP.DotnetnTheBox.Webhock
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        public override void Configure(INancyEnvironment environment)
+        {
+            environment.Tracing(enabled:false, displayErrorTraces:false);
+            base.Configure(environment);
+        }
+    }
+}
