0d5f8c151cb39fc7d38bf18f89578106855b0aaf
Frank Pommerening <frank@pommerening-online.de>
2018-08-18 17:07:05 +0200

    Add Secret sample

---
+++ Samples/Secrets/src/Module/HomeModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace FP.DotnetInTheBox.Secrets.Module
+{
+    public class HomeModule : Nancy.NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", _ =>
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine($"Secret-Sample {DateTime.Now:G}");
+                var databaseconfig = SecretHelper.GetSecret("databaseconfig.cfg");
+                if (string.IsNullOrEmpty(databaseconfig))
+                {
+                    sb.AppendLine("Es wurde keine databaseconfig gefunden");
+                }
+                else
+                {
+                    sb.AppendLine($"databaseconfig:{databaseconfig}");
+                }
+                return sb.ToString();
+            });
+        }
+    }
+}
---
+++ Samples/Secrets/src/Module/SecretHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace FP.DotnetInTheBox.Secrets.Module
+{
+    public class SecretHelper
+    {
+        private const string defaultPath = "/run/secrets/";
+
+        public static string GetSecret(string fileName)
+        {
+            var filePath = Path.Combine(defaultPath, fileName);
+
+#if DEBUG
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+#endif
+
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+            return string.Empty;
+        }
+    }
+}
